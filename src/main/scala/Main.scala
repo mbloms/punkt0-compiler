c2cee12dbb45fb4f9d0ef0465be959322db01cc6
Viktor Kronvall <viktor.kronvall@gmail.com>
2017-02-09 17:49:20 +0100

    Port Lexer to Frege.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,9 +4,9 @@ import cats.syntax.functor._
 object Main {
   import Lexer.{State,NonTerminal,Terminal}
 
-  def main(args: Array[String]) = {
-    lazy val state : State[Int] = Terminal(2)
-    val state2 = state.map(_ + 3)
-    println(state2)
-  }
+  // def main(args: Array[String]) = {
+  //   lazy val state : State[Int] = Terminal(2)
+  //   val state2 = state.map(_ + 3)
+  //   println(state2)
+  // }
 }
