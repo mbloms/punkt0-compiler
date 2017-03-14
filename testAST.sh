df38db0a0313040dde44a7ae90e62890a683697d
Mikael Blomstrand <chawlindel@gmail.com>
2017-03-14 19:05:03 +0100

    Mysigt script.

---
+++ testAST.sh
@@ -0,0 +1,76 @@
+rm out
+echo "99bottles:"
+sbt 'run --ast "/home/micke/Documents/compiler/punkt0-parser-stubs-v2/testprograms/lab3/valid/99bottles.p0"' | tail -n1 > out
+diff ~/Documents/compiler/punkt0-parser-stubs-v2/testprograms/lab3/valid/99bottles.p0.ast out
+rm out
+echo "BinarySearch:"
+sbt 'run --ast "/home/micke/Documents/compiler/punkt0-parser-stubs-v2/testprograms/lab3/valid/BinarySearch.p0"' | tail -n1 > out
+diff ~/Documents/compiler/punkt0-parser-stubs-v2/testprograms/lab3/valid/BinarySearch.p0.ast out
+rm out
+echo "Calendar:"
+sbt 'run --ast "/home/micke/Documents/compiler/punkt0-parser-stubs-v2/testprograms/lab3/valid/Calendar.p0"' | tail -n1 > out
+diff ~/Documents/compiler/punkt0-parser-stubs-v2/testprograms/lab3/valid/Calendar.p0.ast out
+rm out
+echo "ComplexNumbers:"
+sbt 'run --ast "/home/micke/Documents/compiler/punkt0-parser-stubs-v2/testprograms/lab3/valid/ComplexNumbers.p0"' | tail -n1 > out
+diff ~/Documents/compiler/punkt0-parser-stubs-v2/testprograms/lab3/valid/ComplexNumbers.p0.ast out
+rm out
+echo "DrawStuff:"
+sbt 'run --ast "/home/micke/Documents/compiler/punkt0-parser-stubs-v2/testprograms/lab3/valid/DrawStuff.p0"' | tail -n1 > out
+diff ~/Documents/compiler/punkt0-parser-stubs-v2/testprograms/lab3/valid/DrawStuff.p0.ast out
+rm out
+echo "Factorial:"
+sbt 'run --ast "/home/micke/Documents/compiler/punkt0-parser-stubs-v2/testprograms/lab3/valid/Factorial.p0"' | tail -n1 > out
+diff ~/Documents/compiler/punkt0-parser-stubs-v2/testprograms/lab3/valid/Factorial.p0.ast out
+rm out
+echo "GCD:"
+sbt 'run --ast "/home/micke/Documents/compiler/punkt0-parser-stubs-v2/testprograms/lab3/valid/GCD.p0"' | tail -n1 > out
+diff ~/Documents/compiler/punkt0-parser-stubs-v2/testprograms/lab3/valid/GCD.p0.ast out
+rm out
+echo "HeapSort:"
+sbt 'run --ast "/home/micke/Documents/compiler/punkt0-parser-stubs-v2/testprograms/lab3/valid/HeapSort.p0"' | tail -n1 > out
+diff ~/Documents/compiler/punkt0-parser-stubs-v2/testprograms/lab3/valid/HeapSort.p0.ast out
+rm out
+echo "Life:"
+sbt 'run --ast "/home/micke/Documents/compiler/punkt0-parser-stubs-v2/testprograms/lab3/valid/Life.p0"' | tail -n1 > out
+diff ~/Documents/compiler/punkt0-parser-stubs-v2/testprograms/lab3/valid/Life.p0.ast out
+rm out
+echo "Multiplicator:"
+sbt 'run --ast "/home/micke/Documents/compiler/punkt0-parser-stubs-v2/testprograms/lab3/valid/Multiplicator.p0"' | tail -n1 > out
+diff ~/Documents/compiler/punkt0-parser-stubs-v2/testprograms/lab3/valid/Multiplicator.p0.ast out
+rm out
+echo "NewtonsMethod:"
+sbt 'run --ast "/home/micke/Documents/compiler/punkt0-parser-stubs-v2/testprograms/lab3/valid/NewtonsMethod.p0"' | tail -n1 > out
+diff ~/Documents/compiler/punkt0-parser-stubs-v2/testprograms/lab3/valid/NewtonsMethod.p0.ast out
+rm out
+echo "OptimalChange:"
+sbt 'run --ast "/home/micke/Documents/compiler/punkt0-parser-stubs-v2/testprograms/lab3/valid/OptimalChange.p0"' | tail -n1 > out
+diff ~/Documents/compiler/punkt0-parser-stubs-v2/testprograms/lab3/valid/OptimalChange.p0.ast out
+rm out
+echo "Polymorphism:"
+sbt 'run --ast "/home/micke/Documents/compiler/punkt0-parser-stubs-v2/testprograms/lab3/valid/Polymorphism.p0"' | tail -n1 > out
+diff ~/Documents/compiler/punkt0-parser-stubs-v2/testprograms/lab3/valid/Polymorphism.p0.ast out
+rm out
+echo "PrimeTest:"
+sbt 'run --ast "/home/micke/Documents/compiler/punkt0-parser-stubs-v2/testprograms/lab3/valid/PrimeTest.p0"' | tail -n1 > out
+diff ~/Documents/compiler/punkt0-parser-stubs-v2/testprograms/lab3/valid/PrimeTest.p0.ast out
+rm out
+echo "QuickSort:"
+sbt 'run --ast "/home/micke/Documents/compiler/punkt0-parser-stubs-v2/testprograms/lab3/valid/QuickSort.p0"' | tail -n1 > out
+diff ~/Documents/compiler/punkt0-parser-stubs-v2/testprograms/lab3/valid/QuickSort.p0.ast out
+rm out
+echo "ScalarProduct:"
+sbt 'run --ast "/home/micke/Documents/compiler/punkt0-parser-stubs-v2/testprograms/lab3/valid/ScalarProduct.p0"' | tail -n1 > out
+diff ~/Documents/compiler/punkt0-parser-stubs-v2/testprograms/lab3/valid/ScalarProduct.p0.ast out
+rm out
+echo "Simple:"
+sbt 'run --ast "/home/micke/Documents/compiler/punkt0-parser-stubs-v2/testprograms/lab3/valid/Simple.p0"' | tail -n1 > out
+diff ~/Documents/compiler/punkt0-parser-stubs-v2/testprograms/lab3/valid/Simple.p0.ast out
+rm out
+echo "Sudoku:"
+sbt 'run --ast "/home/micke/Documents/compiler/punkt0-parser-stubs-v2/testprograms/lab3/valid/Sudoku.p0"' | tail -n1 > out
+diff ~/Documents/compiler/punkt0-parser-stubs-v2/testprograms/lab3/valid/Sudoku.p0.ast out
+rm out
+echo "VehicleRent:"
+sbt 'run --ast "/home/micke/Documents/compiler/punkt0-parser-stubs-v2/testprograms/lab3/valid/VehicleRent.p0"' | tail -n1 > out
+diff ~/Documents/compiler/punkt0-parser-stubs-v2/testprograms/lab3/valid/VehicleRent.p0.ast out
