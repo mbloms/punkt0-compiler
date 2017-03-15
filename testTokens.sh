ed59211bf4a16cd1f07fd905290e8d37bcc2feb2
Viktor Kronvall <viktor.kronvall@gmail.com>
2017-03-15 23:51:17 +0100

    Run all integration tests with test.sh

--- testTokens.sh
+++ testTokens.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env fish
 for file in ./testprograms/lab2/valid/*.p0
+    echo $file;
     set -l dirname (dirname $file);
     set -l filename (basename $file .p0);
     scala -cp ./target/scala-2.11/classes:./frege3.24.100.jar punkt0.Main --tokens $file | diff - $dirname/$filename.check;
