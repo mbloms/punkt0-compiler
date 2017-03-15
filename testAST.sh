724f7c5eea02fa078f2051b417491695a06ece13
Viktor Kronvall <viktor.kronvall@gmail.com>
2017-03-15 19:02:44 +0100

    ScalaShow class

--- testAST.sh
+++ testAST.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env fish
-rm out
 for file in testprograms/lab3/valid/*.p0;
     echo $file;
     scala -cp ./target/scala-2.12/classes:./frege3.24.100.jar punkt0.Main --ast $file | diff - $file.ast
