f9d04cad07180799ccf8e156e6010dea4ca2da20
Viktor Kronvall <viktor.kronvall@gmail.com>
2017-06-06 18:05:48 +0200

    Update tests to use `java` command

--- testCompile.sh
+++ testCompile.sh
@@ -2,9 +2,9 @@
 for file in testprograms/lab5/valid/*.p0;
 do
     echo $file;
-    scala -cp ./target/scala-2.11/classes:./frege.jar:./lib/cafebabe_2.11-1.2.jar punkt0.Main -d classes $file;
-    java -cp classes Main > our.txt;
-    scala -cp ./punkt0_2.11-1.0.jar:./lib/cafebabe_2.11-1.2.jar punkt0.Main -d classes $file;
-    java -cp classes Main > thiers.txt;
+    scala -cp ./target/scala-2.11/classes:./frege.jar:./lib/cafebabe_2.11-1.2.jar punkt0.Main -d classes $file || exit 1;
+    java -cp classes Main > our.txt || exit 1;
+    scala -cp ./punkt0_2.11-1.0.jar:./lib/cafebabe_2.11-1.2.jar punkt0.Main -d classes $file || exit 0;
+    java -cp classes Main > thiers.txt || exit 0;
     diff our.txt thiers.txt;
 done;
--- testType.sh
+++ testType.sh
@@ -2,17 +2,17 @@
 for file in testprograms/lab7/valid/*.p0;
 do
     echo $file;
-    scala -cp ./target/scala-2.11/classes:./frege.jar punkt0.Main --print --symid $file 2>&1 >/dev/null || exit 1;
+    java -cp ./target/scala-2.11/classes:./frege.jar punkt0.Main --print --symid $file 2>&1 >/dev/null || exit 1;
 done;
 for file in testprograms/lab5/valid/*.p0;
 do
     echo $file;
-    scala -cp ./target/scala-2.11/classes:./frege.jar punkt0.Main --print --symid $file 2>&1 >/dev/null || exit 1;
+    java -cp ./target/scala-2.11/classes:./frege.jar punkt0.Main --print --symid $file 2>&1 >/dev/null || exit 1;
 done;
 for file in testprograms/lab5/invalid/*.p0;
 do
     echo $file;
-    output=`scala -cp ./target/scala-2.11/classes:./frege.jar punkt0.Main --print --symid $file 2>&1 >/dev/null`;
+    output=`java -cp ./target/scala-2.11/classes:./frege.jar punkt0.Main --print --symid $file 2>&1 >/dev/null`;
     if [ $? -ne 1 ]
     then
         echo 'Exit code not 1';
